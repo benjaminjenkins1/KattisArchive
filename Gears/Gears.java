ae244e7c36de083f4b1b4a4125fce1cf90c0b1cf
Benjamin Jenkins <benjamin.jenkins1@marist.edu>
2017-02-24 23:42:36 -0500

    updated Gears solution, wrong answer halfway through

--- Gears/Gears.java
+++ Gears/Gears.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+import java.math.*;
 
 public class Gears{
   public static void main(String[] args){
@@ -16,11 +17,6 @@ public class Gears{
       gearList.add(gear);
     }
 
-    //debug only
-    for(float[] gear : gearList){
-      System.out.println("Read: "+gear[0]+", "+gear[1]+", "+gear[2]+", ");
-    }
-
     boolean isLocked = false;
 
     //set origin turning cw (1) at ratio 1
@@ -30,24 +26,44 @@ public class Gears{
     gearList.get(0)[4] = 1;
 
     for(int i=0; i<gearList.size(); i++){
+      //System.out.println("Examining gear "+i);
       for(int j=0; j<gearList.size(); j++){
         if(i != j){
+          //System.out.println("--Checking gear "+i+" against gear "+j);
           boolean isTangent = checkTangent(gearList.get(i), gearList.get(j));
           if(isTangent && gearList.get(i)[3] != gearList.get(j)[3]){
-            System.out.println("Gear "+i+" is tangent to gear "+j+" and not turning the same direction.");
-            //set direction and ratio
+            //System.out.println("----Gear "+i+" is tangent to gear "+j+" and not turning the same direction.");
+            //set direction
             gearList.get(j)[3] = gearList.get(i)[3] * -1;
+            //System.out.println("------Gear "+j+" direction set to "+gearList.get(j)[3]);
+            //set ratio
             gearList.get(j)[4] = gearList.get(i)[4] * gearList.get(i)[2] /
                 gearList.get(j)[2];
+            //System.out.println("------Gear "+j+" ratio set to "+gearList.get(j)[4]);
           }
           else if(isTangent && gearList.get(i)[3] == gearList.get(j)[3]){
-            System.out.println("Gear "+i+" was turning the same direction as gear "+j+" so the system is now locked.");
+            //System.out.println("----Gear "+i+" was turning the same direction as gear "+j+" so the system is now locked.");
             isLocked = true;
             break;
+          }
           }
         }
       }
     }
+
+    if(isLocked){
+      System.out.println("-1");
+    }
+    else if(gearList.get(gearList.size() - 1)[3] == 0){
+      System.out.println("0");
+    }
+    else{
+      int targetDirection = (int) gearList.get(gearList.size() - 1)[3];
+      float targetRatio = gearList.get(gearList.size() - 1)[4];
+      //System.out.println(targetDirection);
+      System.out.println(formatAnswer(targetDirection, targetRatio));
+    }
+
   }
 
   public static boolean checkTangent(float[] first, float[] second){
@@ -56,18 +72,29 @@ public class Gears{
     float y1 = first[1];
     float y2 = second[1];
     int radiusSum = (int) (first[2]+second[2]);
-    System.out.println("Sum of radii is "+radiusSum);
+    //System.out.println("Sum of radii is "+radiusSum);
     double distance = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
-    System.out.println("Distance is "+distance);
+    //System.out.println("Distance is "+distance);
     int distAsInt = (int) Math.round(distance);
     if(distAsInt == radiusSum){
-      System.out.println("Gear was tangent.");
+      //System.out.println("a gear was tangent.");
       return true;
     }
     else{
-      System.out.println("Gear was not tangent.");
+      //System.out.println("a gear was not tangent.");
       return false;
     }
+  }
+
+  public static String formatAnswer(int direction, float ratio){
+    float factor = 1;
+    for(int i=1; i<100000; i++){
+      if((i * ratio) % 1 == 0){
+        factor = i;
+        break;
+      }
+    }
+    return((int) factor+" "+(int)(factor*ratio*direction));
   }
 }
 
